caec8142b9208e15a836db1eb874d3cb1aaa2386
Francesco Mazzoli <f@mazzo.li>
2015-02-13 17:59:32 +0100

    Add rudimentary support for C quoted embedding
    The conversion from C types to Haskell types is severly incomplete (see
    `cTypeToHsType`).  I also have to think about what to do with
    `typedef`s.
    
    Right now the syntax is just
    
        int        (int x, int y)                      { x + y }
        ^          ^                                   ^
        ret type   parameters capturing Haskell vars   C code using the parameters
    
    I plan to also extend it so that if `x_int` is present in the C code, it
    is automatically added to the list of parameters, so that the above will
    be expressible as
    
        int { x_int + y_int }
    
    or similar.
    
    If a variable appears "declared" multiple times, the types must
    match. So that this is OK
    
        int (int x) { x_int + y_int } ==> int (int x, int y) { x + y }
    
    this is also OK
    
        int { x_int + x_int } ==> int (int x) { x + y }
    
    so is this
    
        int { x_int + x} ==> int (int x) { x + x }
    
    but this is not
    
        int (int x) { x_double + y_int } ==> ?

--- sample-cabal-project/src/Main.hs
+++ sample-cabal-project/src/Main.hs
@@ -3,8 +3,9 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 import           Language.C.Inline
 import qualified Language.Haskell.TH as TH
-import           Language.C.Quote
-import           Language.C.Quote.C
+import qualified Language.C.Quote as C
+import qualified Language.C.Quote.C as C
+import           Foreign.C.Types
 
 emitInclude "math.h"
 
@@ -16,25 +17,25 @@ test_embedCode = c_add 1 2
       [t| Int -> Int -> Int |]    -- Call type
       "francescos_add"            -- Call name
       -- C Code
-      [cunit| int francescos_add(int x, int y) { int z = x + y; return z; } |])
+      [C.cunit| int francescos_add(int x, int y) { int z = x + y; return z; } |])
 
 test_embedStm :: Double
 test_embedStm = $(embedStm
   TH.Unsafe
   [t| Double -> Double |]
-  [cty| double |]
-  [cparams| double x |]
-  [cstm| return cos(x); |]) 1
+  [C.cty| double |]
+  [C.cparams| double x |]
+  [C.cstm| return cos(x); |]) 1
 
 test_embedExp :: Double
 test_embedExp = $(embedExp
   TH.Unsafe
   [t| Double |]
-  [cty| double |]
+  [C.cty| double |]
   []
-  [cexp| sin(1) |])
+  [C.cexp| sin(1) |])
 
-emitCode [cunit|
+emitCode [C.cunit|
 int francescos_mul(int x, int y) {
   return x * y;
 }
@@ -42,9 +43,29 @@ int francescos_mul(int x, int y) {
 
 foreign import ccall "francescos_mul" francescos_mul :: Int -> Int -> Int
 
+test_cexp :: CDouble -> CDouble -> IO CDouble
+test_cexp x y =
+  [cexp| double(double x, double y){ cos(x) + cos(y) } |]
+
+test_cexp_unsafe :: CDouble -> CDouble -> IO CDouble
+test_cexp_unsafe x y =
+  [cexp_unsafe| double(double x, double y){ cos(x) + cos(y) } |]
+
+test_cexp_pure :: CDouble -> CDouble
+test_cexp_pure x =
+  [cexp_pure| double(double x){ cos(x) + sin(x) } |]
+
+test_cexp_pure_unsafe :: CDouble -> CDouble
+test_cexp_pure_unsafe x =
+  [cexp_pure_unsafe| double(double x){ cos(x) + sin(x) } |]
+
 main :: IO ()
 main = do
   print test_embedCode
   print test_embedStm
   print test_embedExp
   print $ francescos_mul 3 4
+  print =<< test_cexp 3 4
+  print =<< test_cexp_unsafe 3 4
+  print $ test_cexp_pure 4
+  print $ test_cexp_pure_unsafe 4
--- src/Language/C/Inline.hs
+++ src/Language/C/Inline.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 -- | Each module that uses at least one of the TH functions below gets
 -- a C file associated to it.  This C file must be built after the
 -- Haskell code and linked appropriately.  If you use cabal, all you
@@ -29,10 +28,22 @@ module Language.C.Inline
   , embedCode
   , embedStm
   , embedExp
+    -- * Quoting
+  , cstm
+  , cstm_unsafe
+  , cstm_pure
+  , cstm_pure_unsafe
+  , cexp
+  , cexp_unsafe
+  , cexp_pure
+  , cexp_pure_unsafe
   ) where
+
+import           Prelude hiding (lex)
 
 import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Syntax as TH
+import qualified Language.Haskell.TH.Quote as TH
 import qualified Text.PrettyPrint.Mainland as PrettyPrint
 import qualified Language.C as C
 import qualified Language.C.Quote.C as C
@@ -46,6 +57,42 @@ import           System.Directory (removeFile)
 import           Data.Functor ((<$>))
 import qualified Data.UUID as UUID
 import qualified Data.UUID.V4 as UUID
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Pos as Parsec
+import qualified Text.Parsec.String as Parsec
+import           Foreign.C.Types
+import           Data.Loc (Pos(..))
+import qualified Data.ByteString.UTF8
+import           Control.Applicative ((*>), (<*))
+
+------------------------------------------------------------------------
+-- Module compile-time state
+
+-- | Records what module we are in.
+{-# NOINLINE currentModuleRef #-}
+currentModuleRef :: IORef (Maybe String)
+currentModuleRef = unsafePerformIO $ newIORef Nothing
+
+-- | Make sure that 'currentModuleRef' and the respective C file are up
+-- to date.
+initialiseModuleState :: TH.Q ()
+initialiseModuleState = do
+  cFile <- cSourceLoc
+  mbModule <- TH.runIO $ readIORef currentModuleRef
+  thisModule <- TH.loc_module <$> TH.location
+  let recordThisModule = TH.runIO $ do
+        -- If the file exists and this is the first time we write
+        -- something from this module (in other words, if we are
+        -- recompiling the module), kill the file first.
+        removeIfExists cFile
+        writeIORef currentModuleRef $ Just thisModule
+  case mbModule of
+    Nothing -> recordThisModule
+    Just currentModule | currentModule == thisModule -> return ()
+    Just _otherModule -> recordThisModule
+
+------------------------------------------------------------------------
+-- Emitting
 
 -- | Data type representing some C code with a typed and named entry
 -- function.
@@ -70,26 +117,8 @@ removeIfExists fileName = removeFile fileName `catch` handleExists
   where
     handleExists e = unless (isDoesNotExistError e) $ throwIO e
 
--- | Make sure that 'currentModuleRef' and the respective C file are up
--- to date.
-initialiseModuleState :: TH.Q ()
-initialiseModuleState = do
-  cFile <- cSourceLoc
-  mbModule <- TH.runIO $ readIORef currentModuleRef
-  thisModule <- TH.loc_module <$> TH.location
-  let recordThisModule = TH.runIO $ do
-        -- If the file exists and this is the first time we write
-        -- something from this module (in other words, if we are
-        -- recompiling the module), kill the file first.
-        removeIfExists cFile
-        writeIORef currentModuleRef $ Just thisModule
-  case mbModule of
-    Nothing -> recordThisModule
-    Just currentModule | currentModule == thisModule -> return ()
-    Just _otherModule -> recordThisModule
-
 -- | Simply appends some string to the module's C file.  Use with care.
-emitLiteral :: String -> TH.Q [TH.Dec]
+emitLiteral :: String -> TH.DecsQ
 emitLiteral s = do
   initialiseModuleState         -- Make sure that things are up-to-date
   cFile <- cSourceLoc
@@ -97,7 +126,7 @@ emitLiteral s = do
   return []
 
 -- | Emits some definition to the module C file.
-emitCode :: [C.Definition] -> TH.Q [TH.Dec]
+emitCode :: [C.Definition] -> TH.DecsQ
 emitCode defs = do
   forM_ defs $ \def -> void $ emitLiteral $ show $ PrettyPrint.ppr def
   return []
@@ -115,11 +144,14 @@ emitCode defs = do
 -- @
 -- emitInclude \<foo\> ==> #include \<foo\>
 -- @
-emitInclude :: String -> TH.Q [TH.Dec]
+emitInclude :: String -> TH.DecsQ
 emitInclude s
   | null s = error "emitImport: empty string"
   | head s == '<' = emitLiteral $ "#include " ++ s
   | otherwise = emitLiteral $ "#include \"" ++ s ++ "\""
+
+------------------------------------------------------------------------
+-- Embedding
 
 -- TODO use the #line CPP macro to have the functions in the C file
 -- refer to the source location in the Haskell file they come from.
@@ -140,11 +172,6 @@ embedCode Code{..} = do
   dec <- TH.forImpD TH.CCall codeCallSafety codeFunName ffiImportName codeType
   TH.addTopDecls [dec]
   [| $(TH.varE ffiImportName) |]
-
--- | Records what module we are in.
-{-# NOINLINE currentModuleRef #-}
-currentModuleRef :: IORef (Maybe String)
-currentModuleRef = unsafePerformIO $ newIORef Nothing
 
 uniqueCName :: IO String
 uniqueCName = do
@@ -206,3 +233,167 @@ embedExp
   -> TH.ExpQ
 embedExp callSafety type_ cRetType cParams cExp =
   embedStm callSafety type_ cRetType cParams [C.cstm| return $exp:cExp; |]
+
+------------------------------------------------------------------------
+-- Quoting sugar
+
+cexp :: TH.QuasiQuoter
+cexp = genericQuote False C.parseExp $ embedExp TH.Safe
+
+cexp_unsafe :: TH.QuasiQuoter
+cexp_unsafe = genericQuote False C.parseExp $ embedExp TH.Unsafe
+
+cexp_pure :: TH.QuasiQuoter
+cexp_pure = genericQuote True C.parseExp $ embedExp TH.Safe
+
+cexp_pure_unsafe :: TH.QuasiQuoter
+cexp_pure_unsafe = genericQuote True C.parseExp $ embedExp TH.Unsafe
+
+cstm :: TH.QuasiQuoter
+cstm = genericQuote False C.parseStm $ embedStm TH.Safe
+
+cstm_unsafe :: TH.QuasiQuoter
+cstm_unsafe = genericQuote False C.parseStm $ embedStm TH.Unsafe
+
+cstm_pure :: TH.QuasiQuoter
+cstm_pure = genericQuote True C.parseStm $ embedStm TH.Safe
+
+cstm_pure_unsafe :: TH.QuasiQuoter
+cstm_pure_unsafe = genericQuote True C.parseStm $ embedStm TH.Unsafe
+
+quoteCode
+  :: (String -> TH.ExpQ)
+  -- ^ The parser
+  -> TH.QuasiQuoter
+quoteCode p = TH.QuasiQuoter
+  { TH.quoteExp = p
+  , TH.quotePat = error "quoteCode: quotePat not implemented"
+  , TH.quoteType = error "quoteCode: quoteType not implemented"
+  , TH.quoteDec = error "quoteCode: quoteDec not implemeted"
+  }
+
+genericQuote
+  :: Bool
+  -- ^ Whether the call should be pure or not
+  -> C.P a
+  -- ^ Parser producing something
+  -> (TH.TypeQ -> C.Type -> [C.Param] -> a -> TH.ExpQ)
+  -- ^ Function taking that something and building an expression, see
+  -- 'embedExp' for other args.
+  -> TH.QuasiQuoter
+genericQuote pure p build = quoteCode $ \s -> do
+  (cType, cParams, cExp) <- runCParser s $ parseTypedC p
+  let cParams' = map cleanupParam cParams
+  let hsType = cFunSigToHsType pure cType $ map snd cParams'
+  buildFunCall (build hsType cType cParams cExp) $ map fst cParams'
+  where
+    buildFunCall :: TH.ExpQ -> [Maybe C.Id] -> TH.ExpQ
+    buildFunCall f [] =
+      f
+    buildFunCall f (mbParamId : params) = case mbParamId of
+      Nothing -> do
+        error "Cannot capture Haskell variable if you don't give a name."
+      Just name -> do
+        mbHsName <- TH.lookupValueName $ case name of
+          C.Id s _ -> s
+          C.AntiId _ _ -> error "inline-c: got antiquotation (buildFunCall)"
+        case mbHsName of
+          Nothing -> do
+            error $ "Cannot capture Haskell variable " ++ show name ++
+                    ", because it's not in scope."
+          Just hsName -> do
+            buildFunCall [| $f $(TH.varE hsName) |] params
+
+    cleanupParam param = case param of
+      C.Param mbId ds d loc -> (mbId, C.Type ds d loc)
+      _                     -> error "inline-c: got antiquotation (inlineExp)"
+-- Type conversion
+
+cFunSigToHsType :: Bool -> C.Type -> [C.Type] -> TH.TypeQ
+cFunSigToHsType pure retType params0 = go params0
+  where
+    go [] = do
+      let hsType = cTypeToHsType retType
+      if pure then hsType else [t| IO $hsType |]
+    go (paramType : params) = do
+      [t| $(cTypeToHsType paramType) -> $(go params) |]
+
+cTypeToHsType :: C.Type -> TH.TypeQ
+cTypeToHsType [C.cty| void |] = [t| () |]
+cTypeToHsType [C.cty| int |] = [t| CInt |]
+cTypeToHsType [C.cty| double |] = [t| CDouble |]
+cTypeToHsType _ = error "TODO cTypeToHsType"
+
+-- cTypeToHsType (C.Type (C.DeclSpec [] [] tySpec noLoc) (C.DeclRoot _) _) =
+--   fromSpec tySpec
+--   where
+--     fromSpec [C.cty| void |] = [t| () |]
+--     fromSpec (C.Tchar Nothing _) = [t| CChar |]
+--     fromSpec (C.Tchar (Just (C.Tsigned _)) _) = [t| CChar |]
+--     fromSpec (C.Tchar (Just (C.Tunsigned _)) _) = [t| CUChar |]
+--     fromSpec _ = error "TODO cTypeToHsType"
+
+
+-- Parsing
+
+runCParser :: String -> Parsec.Parser a -> TH.Q a
+runCParser s p = do
+  loc <- TH.location
+  let (line, col) = TH.loc_start loc
+  let parsecLoc = Parsec.newPos (TH.loc_filename loc) line col
+  let p' = Parsec.setPosition parsecLoc *> p <* Parsec.eof
+  let errOrRes = Parsec.runParser  p' () (TH.loc_filename loc) s
+  case errOrRes of
+    Left err -> do
+      -- TODO consider prefixing with "error while parsing C" or similar
+      error $ show err
+    Right res -> do
+      return res
+
+parseC
+  :: Parsec.SourcePos -> String -> C.P a -> a
+parseC parsecPos str p =
+  let pos = Pos
+        (Parsec.sourceName parsecPos)
+        (Parsec.sourceLine parsecPos)
+        (Parsec.sourceColumn parsecPos)
+        0
+      bytes = Data.ByteString.UTF8.fromString str
+      -- TODO support user-defined C types.
+      pstate = C.emptyPState [C.Antiquotation] [] bytes pos
+  in case C.evalP p pstate of
+    Left err ->
+      -- TODO consider prefixing with "error while parsing C" or similar
+      error $ show err
+    Right x ->
+      x
+
+-- Note that we split the input this way because we cannot compose Happy
+-- parsers easily.
+parseTypedC :: C.P a -> Parsec.Parser (C.Type, [C.Param], a)
+parseTypedC p = do
+  -- Get stuff up to parens, and parse the type
+  typePos <- Parsec.getPosition
+  typeStr <- takeTillChar '('
+  let cType = parseC typePos typeStr C.parseType
+  -- Get stuff for params, and parse the type
+  paramsPos <- Parsec.getPosition
+  paramsStr <- takeTillChar ')'
+  let cParams = parseC paramsPos paramsStr C.parseParams
+  -- Get the body, and feed it to the given parser
+  void $ lex $ Parsec.char '{'
+  bodyPos <- Parsec.getPosition
+  bodyStr <- takeTillChar '}'
+  let cBody = parseC bodyPos bodyStr p
+  -- Whew
+  return (cType, cParams, cBody)
+  where
+    lex p' = do
+      void p'
+      Parsec.spaces
+
+    takeTillChar :: Char -> Parsec.Parser String
+    takeTillChar ch = do
+      str <- Parsec.many1 $ Parsec.satisfy (/= ch)
+      void $ lex $ Parsec.char ch
+      return str
