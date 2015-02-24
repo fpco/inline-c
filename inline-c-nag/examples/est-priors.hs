1419be75ee154303bac66771e627d2fab485045c
Francesco Mazzoli <f@mazzo.li>
2015-02-24 17:46:11 +0100

    Mimic Optimx more closely

--- inline-c-nag/examples/est-priors.hs
+++ inline-c-nag/examples/est-priors.hs
@@ -24,6 +24,12 @@ emitInclude "<nagx02.h>"
 
 type CConst a = a
 
+data Optimx x = Optimx
+  { optimxBest :: !x
+  , optimxMinCost :: !CDouble
+  , optimxCalls :: !CLong
+  } deriving (Eq, Show)
+
 nelderMead
   :: V.Vector CDouble
   -- ^ Starting point
@@ -31,7 +37,7 @@ nelderMead
   -- ^ Function to minimize
   -> CLong
   -- ^ Maximum number of iterations (must be >= 1).
-  -> IO (Maybe (V.Vector CDouble))
+  -> IO (Maybe (Optimx (V.Vector CDouble)))
   -- ^ Position of the minimum.  'Nothing' if something went wrong
   -- (prints the error message)
 nelderMead x pureFunct maxcal = do
@@ -45,26 +51,40 @@ nelderMead x pureFunct maxcal = do
         xFPtr <- newForeignPtr_ xPtr
         let f = pureFunct $ V.unsafeFromForeignPtr0 xFPtr $ fromIntegral n'
         poke fPtr f
-    -- Call the C code
-    res <- alloca $ \fPtr -> VM.unsafeWith xMut $ \xMutPtr -> [citems|
-      int(void (*funct)(Integer n, const double *xc, double *fc, Nag_Comm *comm)) {
-          NagError fail;
-          INIT_FAIL(fail);
-          Nag_Comm comm;
-          double tolf = sqrt(nag_machine_precision);
-          double tolx = sqrt(tolf);
-          nag_opt_simplex_easy(
-            n_nint, xMutPtr_double_ptr, fPtr_double_ptr, tolf, tolx,
-            funct, NULL, maxcal_nint, &comm, &fail);
-          if (fail.code != NE_NOERROR) {
-            printf("Error from nag_opt_simplex_easy (e04cbc).\n%s\n", fail.message);
-          }
-          return fail.code != NE_NOERROR;
-      }
-    |]
-    if res /= 0
-      then return Nothing
-      else Just <$> V.unsafeFreeze xMut
+    alloca $ \fPtr -> alloca $ \ncallsPtr -> VM.unsafeWith xMut $ \xMutPtr -> do
+      -- Create monitoring function, to record number of calls
+      monit <-
+        $(mkFunPtr [t| CDouble -> CDouble -> CConst (CArray CDouble) -> CLong -> CLong -> CDouble -> CDouble -> Ptr Nag_Comm -> IO () |]) $
+        \_fmin _fmax _sim _n ncall _serror _vratio _comm -> poke ncallsPtr ncall
+      -- Call the C code
+      res <- [citems|
+        int(void (*funct)(Integer n, const double *xc, double *fc, Nag_Comm *comm),
+            void (*monit)(double fmin, double fmax, const double sim[], Integer n, Integer ncall, double serror, double vratio, Nag_Comm *comm)) {
+            NagError fail;
+            INIT_FAIL(fail);
+            Nag_Comm comm;
+            double tolf = sqrt(nag_machine_precision);
+            double tolx = sqrt(tolf);
+            nag_opt_simplex_easy(
+              n_nint, xMutPtr_double_ptr, fPtr_double_ptr, tolf, tolx,
+              funct, monit, maxcal_nint, &comm, &fail);
+            if (fail.code != NE_NOERROR) {
+              printf("Error from nag_opt_simplex_easy (e04cbc).\n%s\n", fail.message);
+            }
+            return fail.code != NE_NOERROR;
+        }
+      |]
+      if res /= 0
+        then return Nothing
+        else do
+          f <- peek fPtr
+          x' <- V.unsafeFreeze xMut
+          ncalls <- peek ncallsPtr
+          return $ Just Optimx
+            { optimxBest = x'
+            , optimxMinCost = f
+            , optimxCalls = ncalls
+            }
 
 main :: IO ()
 main = do
@@ -73,4 +93,4 @@ main = do
             x1 = x V.! 1
         in exp x0 * (4*x0*(x0+x1)+2*x1*(x1+1.0)+1.0)
       start = V.fromList [-1, 1]
-  print =<< nelderMead start funct 100
+  print =<< nelderMead start funct 500
