0ca96af3c109fa0eb71aa94a8e0752b8a33ad65c
Francesco Mazzoli <f@mazzo.li>
2015-02-26 10:41:31 +0100

    Make parser more robust.
    Also, switch to trifecta, so that the parser can work with strings directly.

--- inline-c/src/Language/C/Types/Parse.hs
+++ inline-c/src/Language/C/Types/Parse.hs
@@ -1,5 +1,5 @@
 module Language.C.Types.Parse
-  ( Lex.Identifier
+  ( Identifier
   , TypeQual(..)
   , TypeSpec(..)
   , DeclarationSpec(..)
@@ -12,15 +12,17 @@ module Language.C.Types.Parse
   , parseParams
   ) where
 
-import           Text.Parsec
-import           Control.Monad (msum)
-import           Data.Functor ((<$>))
-import           Control.Applicative ((<*>))
-
-import qualified Language.C.Types.Lex as Lex
+import           Control.Monad (msum, void)
+import           Data.Functor ((<$>), (<$))
+import           Control.Applicative ((<*>), (<|>))
+import           Text.Trifecta
+import qualified Data.HashSet as HashSet
+import           Text.Parser.Token.Highlight
 
 ------------------------------------------------------------------------
 -- Types
+
+type Identifier = String
 
 data TypeQual = Const
   deriving (Eq, Show)
@@ -38,7 +40,7 @@ data TypeSpec
   | TypeName String
   deriving (Eq, Show)
 
-data DeclarationSpec = DeclarationSpec [TypeQual] TypeSpec
+data DeclarationSpec = DeclarationSpec [Either TypeQual TypeSpec]
   deriving (Eq, Show)
 
 data Declarator
@@ -50,78 +52,80 @@ data Declarator
 
 type ArraySize = Integer
 
-data Declaration
-  = Declaration DeclarationSpec Declarator
+data Declaration = Declaration DeclarationSpec Declarator
   deriving (Eq, Show)
 
 ------------------------------------------------------------------------
 -- Parse
 
-type Parser = Parsec [(SourcePos, Lex.Token)] ()
-
-parseToken :: Lex.Token -> a -> Parser a
-parseToken tok x =
-  token (Lex.prettyToken . snd) fst $ \(_, tok') ->
-    if tok == tok' then Just x else Nothing
-
-parseIdent :: (Lex.Identifier -> a) -> Parser a
-parseIdent f =
-  token (Lex.prettyToken . snd) fst $ \(_, tok') ->
-    case tok' of
-      Lex.IDENTIFIER s -> Just $ f s
-      _ -> Nothing
-
-parseInteger :: (Integer -> a) -> Parser a
-parseInteger f =
-  token (Lex.prettyToken . snd) fst $ \(_, tok') ->
-    case tok' of
-      Lex.INTEGER s -> Just $ f s
-      _ -> Nothing
+identStyle :: IdentifierStyle Parser
+identStyle = IdentifierStyle
+  { _styleName = "C identifier"
+  , _styleStart = identLetter
+  , _styleLetter = identLetter <|> digit
+  , _styleReserved = HashSet.fromList
+      [ "auto", "else", "long", "switch"
+      , "break", "enum", "register", "typedef"
+      , "case", "extern", "return", "union"
+      , "char", "float", "short", "unsigned"
+      , "const", "for", "signed", "void"
+      , "continue", "goto", "sizeof", "volatile"
+      , "default", "if", "static", "while"
+      , "do", "int", "struct", "double"
+      ]
+  , _styleHighlight = Identifier
+  , _styleReservedHighlight = ReservedIdentifier
+  }
+  where
+    identLetter = oneOf $ ['a'..'z'] ++ ['A'..'Z'] ++ ['_']
 
 parseTypeSpec :: Parser TypeSpec
 parseTypeSpec = msum
-  [ parseToken Lex.VOID Void
-  , parseToken Lex.CHAR Char
-  , parseToken Lex.SHORT Short
-  , parseToken Lex.INT Int
-  , parseToken Lex.LONG Long
-  , parseToken Lex.FLOAT Float
-  , parseToken Lex.DOUBLE Double
-  , parseToken Lex.SIGNED Signed
-  , parseToken Lex.UNSIGNED Unsigned
-  , parseIdent TypeName
+  [ Void <$ reserve identStyle "void"
+  , Char <$ reserve identStyle "char"
+  , Short <$ reserve identStyle "short"
+  , Int <$ reserve identStyle "int"
+  , Long <$ reserve identStyle "long"
+  , Float <$ reserve identStyle "float"
+  , Double <$ reserve identStyle "double"
+  , Signed <$ reserve identStyle "signed"
+  , Unsigned <$ reserve identStyle "unsigned"
+  , TypeName <$> ident identStyle
   ]
 
 parseTypeQual :: Parser TypeQual
 parseTypeQual = msum
-  [ parseToken Lex.CONST Const ]
+  [ Const <$ reserve identStyle "const" ]
 
-parseDeclarationSpec :: Parser DeclarationSpec
-parseDeclarationSpec =
-  DeclarationSpec <$> many parseTypeQual <*> parseTypeSpec
+parseDeclarationSpec
+  :: Parser (DeclarationSpec, Maybe (DeclarationSpec, Identifier))
+parseDeclarationSpec = do
+  let many1 p = (:) <$> p <*> many p
+  qualOrSpecs <- many1 $ (Left <$> parseTypeQual) <|> (Right <$> parseTypeSpec)
+  let mbLastId = case qualOrSpecs of
+        [] -> Nothing
+        _ -> case last qualOrSpecs of
+          Right (TypeName s) -> Just (DeclarationSpec (init qualOrSpecs), s)
+          _ -> Nothing
+  return (DeclarationSpec qualOrSpecs, mbLastId)
 
 -- Intermediate structure to parse damned declarations
 
 data RawDeclarator
-  = RawDeclarator [[TypeQual]] RawDeclaratorRoot [RawDeclaratorTrailing]
-
-data RawDeclaratorRoot
-  = RawDeclaratorRoot String
-  | RawDeclaratorParens Declarator
+  = RawDeclarator [[TypeQual]] Declarator [RawDeclaratorTrailing]
+  deriving (Show, Eq)
 
 data RawDeclaratorTrailing
   = RawDeclaratorArray (Maybe ArraySize)
   | RawDeclaratorProto [Declaration]
+  deriving (Show, Eq)
 
 fromRawDecl :: RawDeclarator -> Declarator
 fromRawDecl (RawDeclarator ptrs0 root trailings0) = goTrailing trailings0
   where
     goPtrs :: [[TypeQual]] -> Declarator
-    goPtrs [] = case root of
-      RawDeclaratorRoot s -> DeclaratorRoot s
-      RawDeclaratorParens decl -> decl
-    goPtrs (quals : ptrs) =
-      Ptr quals $ goPtrs ptrs
+    goPtrs []             = root
+    goPtrs (quals : ptrs) = Ptr quals $ goPtrs ptrs
 
     goTrailing :: [RawDeclaratorTrailing] -> Declarator
     goTrailing [] =
@@ -131,41 +135,59 @@ fromRawDecl (RawDeclarator ptrs0 root trailings0) = goTrailing trailings0
       RawDeclaratorProto decls -> Proto (goTrailing trailings) decls
 
 parseRawDeclarator :: Parser RawDeclarator
-parseRawDeclarator =
-  RawDeclarator <$> many pointer <*> root <*> many trailing
+parseRawDeclarator = do
+  ptrs <- many pointer
+  identOrDec <- root
+  trailings <- many parseRawDeclaratorTrailing
+  return $ case identOrDec of
+    Left s ->
+      RawDeclarator ptrs (DeclaratorRoot s) trailings
+    Right (RawDeclarator ptrs' x trailings') ->
+      RawDeclarator ptrs' (fromRawDecl (RawDeclarator ptrs x trailings)) trailings'
   where
     pointer :: Parser [TypeQual]
     pointer = do
-      parseToken Lex.PTR ()
+      void $ symbolic '*'
       many parseTypeQual
 
-    root :: Parser RawDeclaratorRoot
+    root :: Parser (Either Identifier RawDeclarator)
     root = msum
-      [ do parseIdent RawDeclaratorRoot
-      , do parseToken Lex.LPAREN ()
-           dec <- parseDeclarator
-           parseToken Lex.RPAREN ()
-           return $ RawDeclaratorParens dec
+      [ Left <$> ident identStyle
+      , Right <$> parens parseRawDeclarator
       ]
 
-    trailing :: Parser RawDeclaratorTrailing
-    trailing = msum
-      [ do parseToken Lex.LBRACK ()
-           mbSize <- parseInteger Just <|> return Nothing
-           parseToken Lex.RBRACK ()
-           return $ RawDeclaratorArray mbSize
-      , do parseToken Lex.LPAREN ()
-           decs <- parseParams
-           parseToken Lex.RPAREN ()
-           return $ RawDeclaratorProto decs
-      ]
-
-parseDeclarator :: Parser Declarator
-parseDeclarator = fromRawDecl <$> parseRawDeclarator
+parseRawDeclaratorTrailing :: Parser RawDeclaratorTrailing
+parseRawDeclaratorTrailing = msum
+  [ do mbSize <- brackets $ (Just <$> integer) <|> return Nothing
+       return $ RawDeclaratorArray mbSize
+  , do RawDeclaratorProto <$> parens parseParams
+  ]
 
 parseDeclaration :: Parser Declaration
-parseDeclaration =
-  Declaration <$> parseDeclarationSpec <*> parseDeclarator
+parseDeclaration = do
+  (decSpec, mbLastIdDeclSpec) <- parseDeclarationSpec
+  declaratorOrNoRootDeclarator <-
+    (Left <$> parseRawDeclarator) <|> (Right <$> many parseRawDeclaratorTrailing)
+  (decSpec', declarator) <-
+    case (mbLastIdDeclSpec, declaratorOrNoRootDeclarator) of
+      (_, Left rawDeclarator) ->
+        return (decSpec, fromRawDecl rawDeclarator)
+      (Just (decSpec', s), Right trailings) ->
+        return (decSpec', fromRawDecl $ RawDeclarator [] (DeclaratorRoot s) trailings)
+      (Nothing, Right _) ->
+        fail "Malformed declaration"
+  return $ Declaration decSpec' declarator
 
 parseParams :: Parser [Declaration]
-parseParams = sepBy parseDeclaration $ parseToken Lex.COMMA ()
+parseParams = sepBy parseDeclaration $ symbolic ','
+
+------------------------------------------------------------------------
+-- Proper declaration
+
+-- data PDeclaration = PDeclaration Identifier Type
+
+-- data Type
+--   = TypeSpec TypeSpec
+--   | Ptr [TypeQual] Type
+--   | Array (Maybe ArraySize) Type
+--   | Proto Type [Type]
