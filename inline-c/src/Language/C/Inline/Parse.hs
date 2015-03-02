60540f0de42c2c67cc2056580a8598e400219367
Francesco Mazzoli <francesco@fpcomplete.com>
2015-03-02 11:15:51 +0100

    Integrate new parsing into library.
    Right now tests are disabled.

--- inline-c/src/Language/C/Inline/Parse.hs
+++ inline-c/src/Language/C/Inline/Parse.hs
@@ -4,14 +4,17 @@
 {-# LANGUAGE TypeOperators #-}
 module Language.C.Inline.Parse (runParserInQ, parseTypedC) where
 
-import           Control.Applicative ((<*), (<|>))
+import           Control.Applicative ((<*), (*>), (<|>))
 import           Control.Monad (void, msum)
-import qualified Data.ByteString.UTF8 as UTF8
 import qualified Data.Map as Map
 import qualified Language.Haskell.TH as TH
-import qualified Text.Trifecta as Trifecta
-import qualified Text.Trifecta.Delta as Trifecta
-import qualified Text.Parser.LookAhead as Trifecta
+import qualified Text.Parser.Char as Parser
+import qualified Text.Parser.LookAhead as Parser
+import qualified Text.Parser.Combinators as Parser
+import qualified Text.Parser.Token as Parser
+import           Text.Parsec.String (Parser)
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Pos as Parsec
 import           Data.Monoid ((<>))
 import           Text.PrettyPrint.ANSI.Leijen ((<+>))
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
@@ -21,60 +24,61 @@ import qualified Language.C.Types as C
 
 -- Parsing
 
-runParserInQ :: String -> Trifecta.Parser a -> TH.Q a
+runParserInQ :: String -> Parser a -> TH.Q a
 runParserInQ s p = do
   loc <- TH.location
   let (line, col) = TH.loc_start loc
-  let d = Trifecta.Directed
-        (UTF8.fromString (TH.loc_filename loc)) (fromIntegral line)
-        (fromIntegral col) 0 0
-  case Trifecta.parseString (p <* Trifecta.eof) d s of
-    Trifecta.Failure err -> do
+  let parsecLoc = Parsec.newPos (TH.loc_filename loc) line col
+  let p' = Parsec.setPosition parsecLoc *> p <* Parsec.eof
+  let errOrRes = Parsec.runParser  p' () (TH.loc_filename loc) s
+  case errOrRes of
+    Left err -> do
       -- TODO consider prefixing with "error while parsing C" or similar
-      error $ pretty80 err
-    Trifecta.Success res -> do
+      error $ show err
+    Right res -> do
       return res
 
 parseTypedC
-  :: Trifecta.Parser (C.Declaration (), [C.Declaration C.Id], String)
+  :: Parser (C.Declaration (), [C.Declaration C.Id], String)
   -- ^ Returns the retun type, the captured variables, and the body.
 parseTypedC = do
-  -- Parse return type
+  -- Parse return type (consume spaces first)
+  Parser.spaces
   cRetType <- C.parseAbstractDeclaration
   -- Parse the body
-  void $ Trifecta.char '{'
+  void $ Parser.char '{'
   (cParams, cBody) <- parseBody
   -- Massage the params to make sure that we're good
   cParams' <- processParams cParams
   return (cRetType, cParams', cBody)
   where
-    parseBody :: Trifecta.Parser ([Either C.Id (C.Declaration C.Id)], String)
+    parseBody :: Parser ([Either C.Id (C.Declaration C.Id)], String)
     parseBody = do
       -- Note that this code does not work with "lexing" combinators
       -- (apart when appropriate) because we want to make sure to
       -- preserve whitespace after the things we substitute.
-      s <- Trifecta.manyTill Trifecta.anyChar $
-           Trifecta.lookAhead (Trifecta.char '}' <|> Trifecta.char '$')
+      s <- Parser.manyTill Parser.anyChar $
+           Parser.lookAhead (Parser.char '}' <|> Parser.char '$')
       let parseEscapedDollar = do
-            void $ Trifecta.char '$'
+            void $ Parser.char '$'
             return ([], "$")
       let parseTypedCapture = do
-            void $ Trifecta.symbolic '('
+            void $ Parser.symbolic '('
             decl@(C.Declaration s' _ _) <- C.parseDeclaration
-            void $ Trifecta.char ')'
+            void $ Parser.char ')'
             return ([Right decl], s')
       let parseCapture = do
             s' <- C.parseIdentifier
             return ([Left s'], s')
       (decls, s') <- msum
-        [ do Trifecta.try $ do -- Try because we might fail to parse the 'eof'
-               void $ Trifecta.symbolic '}'
-               Trifecta.eof
+        [ do Parser.try $ do -- Try because we might fail to parse the 'eof'
+               void $ Parser.symbolic '}'
+               Parser.eof
              return ([], "")
-        , do void $ Trifecta.char '}'
+        , do void $ Parser.char '}'
              (decls, s') <- parseBody
              return (decls, "}" ++ s')
-        , do void $ Trifecta.char '$'
+        , do void $ Parser.char '$'
              (decls1, s1) <- parseEscapedDollar <|> parseTypedCapture <|> parseCapture
              (decls2, s2) <- parseBody
              return (decls1 ++ decls2, s1 ++ s2)
@@ -83,7 +87,7 @@ parseTypedC = do
 
     processParams
       :: [Either C.Id (C.Declaration C.Id)]
-      -> Trifecta.Parser [C.Declaration C.Id]
+      -> Parser [C.Declaration C.Id]
     processParams cParams = do
       let (untyped, typed) = partitionEithers cParams
       params <- go Map.empty typed
@@ -91,7 +95,7 @@ parseTypedC = do
       return $ map snd $ Map.toList params
       where
         go :: Map.Map C.Id (C.Declaration C.Id) -> [C.Declaration C.Id]
-           -> Trifecta.Parser (Map.Map C.Id (C.Declaration C.Id))
+           -> Parser (Map.Map C.Id (C.Declaration C.Id))
         go visitedParams [] = do
           return visitedParams
         go visitedParams (decl@(C.Declaration s quals ty) : params) = do
@@ -105,7 +109,7 @@ parseTypedC = do
                 "definition conflicts with new type:" <+> PP.pretty decl'
 
         checkUntyped
-          :: Map.Map C.Id (C.Declaration C.Id) -> [C.Id] -> Trifecta.Parser ()
+          :: Map.Map C.Id (C.Declaration C.Id) -> [C.Id] -> Parser ()
         checkUntyped params = mapM_ $ \s -> do
           case Map.lookup s params of
             Just _ -> return ()
