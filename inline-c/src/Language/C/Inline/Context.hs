d39d34e171c98ce4f923a210498dafada78f0b39
Francesco Mazzoli <francesco@fpcomplete.com>
2015-03-06 15:37:17 +0100

    Make IO marshalling more flexible

--- inline-c/src/Language/C/Inline/Context.hs
+++ inline-c/src/Language/C/Inline/Context.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
 -- | A 'Context' is used to define the capabilities of the
 -- TemplateHaskell code that handles the inline C code.  See the
 -- documentation of the data type for more details.
@@ -8,7 +11,10 @@
 -- that defines new C types, to allow the TemplateHaskell code to
 -- interpret said types correctly.
 module Language.C.Inline.Context
-  ( Context(..)
+  ( Purity(..)
+  , BaseMarshaller
+  -- , Marshaller
+  , Context(..)
   , CArray
   , convertCType
   , isTypeName
@@ -25,12 +31,23 @@ import           Foreign.C.Types
 import           Foreign.Ptr (Ptr, FunPtr)
 import qualified Language.Haskell.TH as TH
 import           Data.Functor ((<$>))
-import           System.IO.Unsafe (unsafePerformIO)
 import qualified Data.Map as Map
 import           Data.Monoid ((<>))
 
 import           Language.C.Inline.FunPtr
 import qualified Language.C.Types as C
+
+data Purity = Pure | IO
+
+type BaseMarshaller =
+  Purity -> TH.Type -> TH.Exp -> TH.Q (Maybe TH.Exp)
+
+-- data Marshaller a = Marshaller
+--   { marshallerParser :: forall m. C.CParser m => m a
+--   , marshaller :: Purity -> a -> TH.Q (TH.Type, TH.Exp)
+--   }
+
+-- type MarshallerId = String
 
 -- | A 'Context' stores information needed to convert C types to Haskell
 -- types, and to convert Haskell values to C values.
@@ -39,52 +56,60 @@ import qualified Language.C.Types as C
 -- 'mappend' is right-biased -- in @'mappend' x y@ @y@ will take
 -- precedence over @x@.
 data Context = Context
-  { ctxConvertCTypeSpec :: Map.Map C.TypeSpecifier TH.TypeQ
+  { ctxCTypesTable :: Map.Map C.TypeSpecifier TH.TypeQ
     -- ^ Maps 'TypeSpecifier's to Haskell types.  This is needed to
     -- convert C types to Haskell types, and to parse C types
     -- correctly.
     --
     -- Note that 'C.TypeSpecifier's that are not in the 'Context' map
     -- might not be parsed correctly.
-  , ctxMarshaller :: TH.Type -> TH.Exp -> TH.Q (Maybe TH.Exp)
-    -- ^ The marshaller takes the expected type of a captured variable
-    -- and the 'TH.Exp' representing the captured variable itself and
-    -- processes it into something else.
-    --
-    -- In 'baseCtx', the marshaller simply returns the expression
-    -- unchanged, but for example the 'funPtrCtx' processes 'FunPtr'
-    -- arguments so that Haskell functions are automatically converted
-    -- to 'FunPtr's.
+  , ctxBaseMarshaller :: BaseMarshaller
+  -- , ctxMarshallers :: Map.Map MarshallerId Marshaller
+  -- , ctxMarshaller :: TH.Type -> TH.Exp -> TH.Q (Maybe TH.Exp)
+  --   -- ^ The marshaller takes the expected type of a captured variable
+  --   -- and the 'TH.Exp' representing the captured variable itself and
+  --   -- processes it into something else.
+  --   --
+  --   -- In 'baseCtx', the marshaller simply returns the expression
+  --   -- unchanged, but for example the 'funPtrCtx' processes 'FunPtr'
+  --   -- arguments so that Haskell functions are automatically converted
+  --   -- to 'FunPtr's.
   }
 
 instance Monoid Context where
   mempty = Context
-    { ctxConvertCTypeSpec = Map.empty
-    , ctxMarshaller = \_ _ -> runMaybeT empty
+    { ctxCTypesTable = Map.empty
+    , ctxBaseMarshaller = \_ _ _ -> runMaybeT empty
+    -- , ctxMarshallers = mempty
     }
 
   mappend ctx2 ctx1 = Context
-    { ctxConvertCTypeSpec =
-        ctxConvertCTypeSpec ctx1 <> ctxConvertCTypeSpec ctx2
-    , ctxMarshaller = \hsTy hsArg -> runMaybeT $
-        MaybeT (ctxMarshaller ctx1 hsTy hsArg) <|> MaybeT (ctxMarshaller ctx2 hsTy hsArg)
+    { ctxCTypesTable =
+        ctxCTypesTable ctx1 <> ctxCTypesTable ctx2
+    , ctxBaseMarshaller = \purity hsTy hsExp -> runMaybeT $
+        MaybeT (ctxBaseMarshaller ctx1 purity hsTy hsExp) <|>
+        MaybeT (ctxBaseMarshaller ctx2 purity hsTy hsExp)
+    -- , ctxMarshallers = ctxMarshallers ctx1 <> ctxMarshallers ctx2
     }
 
 -- | Context useful to work with vanilla C.  Used by default.
 --
--- 'ctxConvertCTypeSpec': converts C basic types to their counterparts
+-- 'ctxCTypesTable': converts C basic types to their counterparts
 -- in "Foreign.C.Types" (TODO currently slightly incomplete).
 --
 -- 'ctxMarshaller': Just returns the expression.
 -- @
 baseCtx :: Context
-baseCtx = Context
-  { ctxConvertCTypeSpec = baseConvertCTypeSpec
-  , ctxMarshaller = \_ hsExp -> return $ Just hsExp
+baseCtx = mempty
+  { ctxCTypesTable = baseCTypesTable
+  , ctxBaseMarshaller = \purity _ hsExp -> case purity of
+      Pure -> return $ Just hsExp
+      IO -> Just <$> [| \cont -> cont $(return hsExp) |]
+  -- , ctxMarshallers = Map.empty
   }
 
-baseConvertCTypeSpec :: Map.Map C.TypeSpecifier TH.TypeQ
-baseConvertCTypeSpec = Map.fromList
+baseCTypesTable :: Map.Map C.TypeSpecifier TH.TypeQ
+baseCTypesTable = Map.fromList
   [ (C.Void, [t| () |])
   , (C.Char Nothing, [t| CChar |])
   , (C.Char (Just C.Signed), [t| CChar |])
@@ -107,11 +132,11 @@ type CArray = Ptr
 ------------------------------------------------------------------------
 -- Type conversion
 
--- | Given a 'Context', it uses its 'ctxConvertCTypeSpec' to convert
+-- | Given a 'Context', it uses its 'ctxCTypesTable' to convert
 -- arbitrary C types.
 convertCType
   :: Context
-  -> Bool
+  -> Purity
   -- ^ Whether function pointers should be pure or not.
   -> C.Type
   -> TH.Q (Maybe TH.Type)
@@ -122,7 +147,7 @@ convertCType ctx pure = runMaybeT . go
     go :: C.Type -> MaybeT TH.Q TH.Type
     go cTy = case cTy of
       C.TypeSpecifier _specs cSpec ->
-        case Map.lookup cSpec (ctxConvertCTypeSpec ctx) of
+        case Map.lookup cSpec (ctxCTypesTable ctx) of
           Nothing -> mzero
           Just ty -> lift ty
       C.Ptr _quals (C.Proto retType pars) -> do
@@ -139,13 +164,14 @@ convertCType ctx pure = runMaybeT . go
         -- We cannot convert standalone prototypes
         mzero
 
-    buildArr [] hsRetType =
-      if pure then [t| $(return hsRetType) |] else [t| IO $(return hsRetType) |]
+    buildArr [] hsRetType = case pure of
+      Pure -> [t| $(return hsRetType) |]
+      IO -> [t| IO $(return hsRetType) |]
     buildArr (hsPar : hsPars) hsRetType =
       [t| $(return hsPar) -> $(buildArr hsPars hsRetType) |]
 
 isTypeName :: Context -> C.Id -> Bool
-isTypeName ctx id' = Map.member (C.TypeName id') $ ctxConvertCTypeSpec ctx
+isTypeName ctx id' = Map.member (C.TypeName id') $ ctxCTypesTable ctx
 
 ------------------------------------------------------------------------
 -- Function pointer removal
@@ -160,13 +186,11 @@ isTypeName ctx id' = Map.member (C.TypeName id') $ ctxConvertCTypeSpec ctx
 -- Using the 'funPtrCtx' captured things of type @'Int' -> 'Int' -> 'IO'
 -- 'Int'@ will automatically be converted to a 'FunPtr'.
 funPtrCtx :: Context
-funPtrCtx = mempty{ctxMarshaller = convertFunPtrs}
+funPtrCtx = mempty{ctxBaseMarshaller = convertFunPtrs}
   where
-    convertFunPtrs :: TH.Type -> TH.Exp -> TH.Q (Maybe TH.Exp)
-    convertFunPtrs hsTy hsExp = case hsTy of
-      -- TODO I think this 'unsafePerformIO' is safe, given that the
-      -- arguments to the FFI call are passed immediately and do not
-      -- leak anywhere else.  But check.
-      TH.AppT (TH.ConT n) hsTy' | n == ''FunPtr ->
-        Just <$> [| unsafePerformIO ($(mkFunPtr (return hsTy')) $(return hsExp)) |]
+    convertFunPtrs :: BaseMarshaller
+    convertFunPtrs purity hsTy hsExp = case hsTy of
+      TH.AppT (TH.ConT n) hsTy' | n == ''FunPtr -> case purity of
+        IO -> Just <$> [| \cont -> cont =<< $(mkFunPtr (return hsTy')) $(return hsExp) |]
+        Pure -> error "Cannot convert functions to pointers in pure quotation (funPtrCtx)"
       _ -> return Nothing
