2063dbe5f5476dbb0da5ae09a7bd5aa64834cec9
Francesco Mazzoli <francesco@fpcomplete.com>
2015-03-11 10:37:18 +0100

    Make core module smaller, for greater clarity.

--- inline-c/src/Language/C/Inline.hs
+++ inline-c/src/Language/C/Inline.hs
@@ -28,60 +28,34 @@ module Language.C.Inline
     , c_unsafe
     , c_pure
     , c_pure_unsafe
+    , include
 
       -- * 'FunPtr' utils
       --
-      -- | The functions in this section provide facilities to quickly
-      -- get 'FunPtr's from Haskell functions, and vice-versa.
+      -- | Functions to quickly convert from/to 'FunPtr's.  They're provided
+      -- here since they can be useful to work with Haskell functions
+      -- in C, and vice-versa.  However, consider using 'funCtx' if you're
+      -- doing this a lot.
     , mkFunPtr
     , peekFunPtr
 
-      -- * Low-level API
-      --
-      -- | The functions in this section let us access more the C file
-      -- associated with the current module.  They can be used to build
-      -- additional features on top of the basic machinery.
-
-      -- ** Emitting C code
-    , emitLiteral
-    , include
-
-      -- ** Inlining C code
-      -- $embedding
-    , Code(..)
-    , inlineCode
-    , inlineExp
-    , inlineItems
-
-      -- ** Re-exports
+      -- * Re-exports
     , module Foreign.C.Types
     , Ptr
     , FunPtr
     ) where
 
-import           Control.Exception (catch, throwIO)
-import           Control.Monad (void, unless, forM)
-import qualified Crypto.Hash as CryptoHash
-import qualified Data.Binary as Binary
-import           Data.Foldable (forM_)
-import           Data.Functor ((<$>))
-import           Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import           Control.Monad (forM)
 import qualified Data.Map as Map
-import           Data.Maybe (fromMaybe)
 import           Foreign.C.Types
 import           Foreign.Ptr (Ptr, FunPtr)
 import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Quote as TH
-import qualified Language.Haskell.TH.Syntax as TH
-import           System.Directory (removeFile)
-import           System.FilePath (addExtension, dropExtension)
-import           System.IO.Error (isDoesNotExistError)
-import           System.IO.Unsafe (unsafePerformIO)
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
 import qualified Language.C.Types as C
 import           Language.C.Inline.Context
-import           Language.C.Inline.Parse
+import           Language.C.Inline.Internal
 import           Language.C.Inline.FunPtr
 
 ------------------------------------------------------------------------
@@ -126,262 +100,6 @@ import           Language.C.Inline.FunPtr
 -- $ cc -c Bar.c -o Bar_c.o
 -- $ ghc Main.o Foo.o Bar.o Main_c.o Bar_c.o -lm -o Main
 -- @
-data ModuleState = ModuleState
-  { msModuleName :: String
-  , msContext :: Context
-  , msGeneratedNames :: Int
-  }
-
-{-# NOINLINE moduleStateRef #-}
-moduleStateRef :: IORef (Maybe ModuleState)
-moduleStateRef = unsafePerformIO $ newIORef Nothing
-
--- | Make sure that 'moduleStateRef' and the respective C file are up
--- to date.
-initialiseModuleState
-  :: Maybe Context
-  -- ^ The 'Context' to use if we initialise the module.  If 'Nothing',
-  -- 'baseCtx' will be used.
-  -> TH.Q ()
-initialiseModuleState mbContext = do
-  cFile <- cSourceLoc
-  mbModuleState <- TH.runIO $ readIORef moduleStateRef
-  thisModule <- TH.loc_module <$> TH.location
-  let recordThisModule = TH.runIO $ do
-        -- If the file exists and this is the first time we write
-        -- something from this module (in other words, if we are
-        -- recompiling the module), kill the file first.
-        removeIfExists cFile
-        writeIORef moduleStateRef $ Just ModuleState
-          { msModuleName = thisModule
-          , msContext = context
-          , msGeneratedNames = 0
-          }
-  case mbModuleState of
-    Nothing -> recordThisModule
-    Just ms | msModuleName ms == thisModule -> return ()
-    Just _ms -> recordThisModule
-  where
-    context = fromMaybe baseCtx mbContext
-
-initialiseModuleState_ :: TH.Q ()
-initialiseModuleState_ = initialiseModuleState Nothing
-
-getModuleState :: TH.Q ModuleState
-getModuleState = do
-  mbModuleState <- TH.runIO $ readIORef moduleStateRef
-  thisModule <- TH.loc_module <$> TH.location
-  case mbModuleState of
-    Nothing -> error "inline-c: ModuleState not present"
-    Just ms | msModuleName ms == thisModule -> return ms
-    Just _ms -> error "inline-c: stale ModuleState"
-
-getContext :: TH.Q Context
-getContext = msContext <$> getModuleState
-
--- $context
---
--- The inline C functions ('cexp', 'c', etc.) need a 'Context' to
--- operate.  Said context can be explicitely set with 'setContext'.
--- Otherwise, at the first usage of one of the TH functions in this
--- module the 'Context' is implicitely set to 'baseCtx'.
-
--- | Sets the 'Context' for the current module.  This function, if
--- called, must be called before any of the other TH functions in this
--- module.  Fails if that's not the case.
-setContext :: Context -> TH.DecsQ
-setContext ctx = do
-  mbModuleState <- TH.runIO $ readIORef moduleStateRef
-  forM_ mbModuleState $ \_moduleState -> do
-    error "inline-c: The module has already been initialised (setContext)."
-  initialiseModuleState $ Just ctx
-  return []
-
-bumpGeneratedNames :: TH.Q Int
-bumpGeneratedNames = do
-  ms <- getModuleState
-  TH.runIO $ do
-    let c' = msGeneratedNames ms
-    writeIORef moduleStateRef $ Just ms{msGeneratedNames = c' + 1}
-    return c'
-
-------------------------------------------------------------------------
--- Emitting
-
-cSourceLoc :: TH.Q FilePath
-cSourceLoc = do
-  thisFile <- TH.loc_filename <$> TH.location
-  return $ dropExtension thisFile `addExtension` "c"
-
-removeIfExists :: FilePath -> IO ()
-removeIfExists fileName = removeFile fileName `catch` handleExists
-  where
-    handleExists e = unless (isDoesNotExistError e) $ throwIO e
-
--- | Simply appends some string to the module's C file.  Use with care.
-emitLiteral :: String -> TH.DecsQ
-emitLiteral s = do
-  initialiseModuleState_         -- Make sure that things are up-to-date
-  cFile <- cSourceLoc
-  TH.runIO $ appendFile cFile $ "\n" ++ s ++ "\n"
-  return []
-
--- | Emits an include CPP statement for the given file.
--- To avoid having to escape quotes, the function itself adds them when
--- appropriate, so that
---
--- @
--- include "foo.h" ==> #include "foo.h"
--- @
---
--- but
---
--- @
--- include \<foo\> ==> #include \<foo\>
--- @
-include :: String -> TH.DecsQ
-include s
-  | null s = error "inline-c: empty string (include)"
-  | head s == '<' = emitLiteral $ "#include " ++ s
-  | otherwise = emitLiteral $ "#include \"" ++ s ++ "\""
-
-------------------------------------------------------------------------
--- Inlining
-
--- $embedding
---
--- We use the 'Code' data structure to represent some C code that we
--- want to emit to the module's C file and immediately generate a
--- foreign call to.  For this reason, 'Code' includes both some C
--- definition, and enough information to be able to generate a foreign
--- call -- specifically the name of the function to call and the Haskell
--- type.
---
--- All the quasi-quoters work by constructing a 'Code' and calling
--- 'inlineCode'.
-
--- | Data type representing a list of C definitions with a typed and named entry
--- function.
---
--- We use it as a basis to inline and call C code.
-data Code = Code
-  { codeCallSafety :: TH.Safety
-    -- ^ Safety of the foreign call
-  , codeType :: TH.TypeQ
-    -- ^ Type of the foreign call
-  , codeFunName :: String
-    -- ^ Name of the function to call in the code below.
-  , codeDefs :: String
-    -- ^ The C code.
-  }
-
--- TODO use the #line CPP macro to have the functions in the C file
--- refer to the source location in the Haskell file they come from.
---
--- See <https://gcc.gnu.org/onlinedocs/cpp/Line-Control.html>.
-
--- | Inlines a piece of code inline.  The resulting 'TH.Exp' will have
--- the type specified in the 'codeType'.
---
--- In practice, this function outputs the C code to the module's C file,
--- and then inserts a foreign call of type 'codeType' calling the
--- provided 'codeFunName'.
---
--- Example:
---
--- @
--- c_add :: Int -> Int -> Int
--- c_add = $(inlineCode $ Code
---   TH.Unsafe                   -- Call safety
---   [t| Int -> Int -> Int |]    -- Call type
---   "francescos_add"            -- Call name
---   -- C Code
---   [C.cunit| int francescos_add(int x, int y) { int z = x + y; return z; } |])
--- @
-inlineCode :: Code -> TH.ExpQ
-inlineCode Code{..} = do
-  initialiseModuleState_         -- Make sure that things are up-to-date
-  -- Write out definitions
-  void $ emitLiteral codeDefs
-  -- Create and add the FFI declaration.
-  ffiImportName <- uniqueFfiImportName
-  dec <- TH.forImpD TH.CCall codeCallSafety codeFunName ffiImportName codeType
-  TH.addTopDecls [dec]
-  TH.varE ffiImportName
-
-uniqueCName :: String -> TH.Q String
-uniqueCName x = do
-  c' <- bumpGeneratedNames
-  let unique :: CryptoHash.Digest CryptoHash.SHA1 = CryptoHash.hashlazy $ Binary.encode x
-  return $ "inline_c_" ++ show c' ++ "_" ++ show unique
-
--- | Same as 'inlineItems', but with a single expression.
---
--- @
--- c_cos :: Double -> Double
--- c_cos = $(inlineExp
---   TH.Unsafe
---   [t| Double -> Double |]
---   [cty| double |] [cparams| double x |]
---   [cexp| cos(x) |])
--- @
-inlineExp
-  :: TH.Safety
-  -- ^ Safety of the foreign call
-  -> TH.TypeQ
-  -- ^ Type of the foreign call
-  -> C.Type
-  -- ^ Return type of the C expr
-  -> [(C.Id, C.Type)]
-  -- ^ Parameters of the C expr
-  -> String
-  -- ^ The C expression
-  -> TH.ExpQ
-inlineExp callSafety type_ cRetType cParams cExp =
-  inlineItems callSafety type_ cRetType cParams cItems
-  where
-    cItems = case cRetType of
-      C.TypeSpecifier _quals C.Void -> cExp ++ ";"
-      _ -> "return (" ++ cExp ++ ");"
-
--- | Same as 'inlineCode', but accepts a list of 'C.BlockItem's instead than a
--- full-blown 'Code'.  A function containing the provided statement will be
--- automatically generated.
---
--- @
--- c_cos :: Double -> Double
--- c_cos = $(inlineItems
---   TH.Unsafe
---   [t| Double -> Double |]
---   [cty| double |] [cparams| double x |]
---   [c| return cos(x); |])
--- @
-inlineItems
-  :: TH.Safety
-  -- ^ Safety of the foreign call
-  -> TH.TypeQ
-  -- ^ Type of the foreign call
-  -> C.Type
-  -- ^ Return type of the C expr
-  -> [(C.Id, C.Type)]
-  -- ^ Parameters of the C expr
-  -> String
-  -- ^ The C items
-  -> TH.ExpQ
-inlineItems callSafety type_ cRetType cParams cItems = do
-  let mkParam (id', paramTy) = C.ParameterDeclaration (Just id') paramTy
-  let proto = C.Proto cRetType (map mkParam cParams)
-  funName <- uniqueCName $ show proto ++ cItems
-  let decl = C.ParameterDeclaration (Just (C.Id funName)) proto
-  let defs =
-        prettyOneLine decl ++ " {\n" ++
-        cItems ++ "\n}\n"
-  inlineCode $ Code
-    { codeCallSafety = callSafety
-    , codeType = type_
-    , codeFunName = funName
-    , codeDefs = defs
-    }
 
 ------------------------------------------------------------------------
 -- Quoting sugar
@@ -409,17 +127,19 @@ inlineItems callSafety type_ cRetType cParams cItems = do
 --   the C code.  If no parameters are present, the parentheses can be
 --   omitted.
 --
--- * The syntax of the @\<C code\>@ depends on the quasi-quoter used.
---   @cexp@ functions accept a C expression.  @c@ functions accept
---   'C.BlockItem's, which are basically what goes inside a C
---   function.
+-- * The syntax of the @\<C code\>@ depends on on the quasi-quoter used,
+--   and the anti-quoters available.  @cexp@ functions accept a C
+--   expression.  @c@ functions accept a list of statemens, like the body
+--   of a function.
+--
+-- See also the @tutorial.md@ file for more documentation.
 --
 -- === Variable capturing and type conversion.
 --
 -- The Haskell type of the inlined expression will be determined by the
 -- C return type specified.  The conversion between the C type and the
 -- Haskell type is performed according to the current 'Context' -- see
--- 'ctxConvertCTypeSpec'.  C pointers and arrays are both converted to
+-- 'convertCType'.  C pointers and arrays are both converted to
 -- Haskell @'Ptr'@s, and function pointers are converted to @'FunPtr'@s.
 -- Sized arrays are not supported.
 --
@@ -428,6 +148,22 @@ inlineItems callSafety type_ cRetType cParams cItems = do
 -- to the C type provided.  For example, if we capture variable @x@
 -- using @double x@ in the parameter list, the code will expect a
 -- variable @x@ of type @CDouble@ in Haskell.
+--
+-- === Anti-quoters
+--
+-- Apart from using parameter lists, Haskell variables can be captured
+-- using anti-quoters.  @inline-c@ provides a basic anti-quoting
+-- mechanism extensible with user-defined anti-quoters (see
+-- "Language.C.Inline.Context").  The basic anti-quoter lets you capture
+-- Haskell variables on the fly, for example we might say
+--
+-- @
+-- ['cexp'| double { cos($(double x)) } |]
+-- @
+--
+-- Which would capture the Haskell variable @x@ of type @'CDouble'@.
+--
+-- Parameter list capturing and anti-quoting can be freely mixed.
 --
 -- === @pure@ and impure calls
 --
@@ -466,7 +202,7 @@ inlineItems callSafety type_ cRetType cParams cItems = do
 -- 'include' "\<math.h\>"
 --
 -- c_cos :: 'CDouble' -> 'CDouble'
--- c_cos x = ['cexp_pure_unsafe'| double(double x) { cos(x) } |]
+-- c_cos x = ['cexp_pure_unsafe'| double { cos($(double x)) } |]
 -- @
 --
 -- === Inline C statements
@@ -483,9 +219,9 @@ inlineItems callSafety type_ cRetType cParams cItems = do
 -- parseVector :: 'CInt' -> 'IO' (V.IOVector 'CDouble')
 -- parseVector len = do
 --   vec <- V.new $ 'fromIntegral' len0
---   V.unsafeWith vec $ \\ptr -> ['c'| void(int len, double *ptr) {
+--   V.unsafeWith vec $ \\ptr -> ['c'| void(double *ptr) {
 --     int i;
---     for (i = 0; i < len; i++) {
+--     for (i = 0; i < $(int len); i++) {
 --       scanf("%lf ", &ptr[i]);
 --     }
 --   } |]
@@ -600,11 +336,27 @@ genericQuote pure build = quoteCode $ \s -> do
         go (paramType : params) = do
           [t| $(return paramType) -> $(go params) |]
 
+-- | Emits an include CPP statement for C code associated with the current
+-- module.  To avoid having to escape quotes, the function itself adds
+-- them when appropriate, so that
+--
+-- @
+-- include "foo.h" ==> #include "foo.h"
+-- @
+--
+-- but
+--
+-- @
+-- include "\<foo\>" ==> #include \<foo\>
+-- @
+include :: String -> TH.DecsQ
+include s
+  | null s = error "inline-c: empty string (include)"
+  | head s == '<' = emitLiteral $ "#include " ++ s
+  | otherwise = emitLiteral $ "#include \"" ++ s ++ "\""
+
 ------------------------------------------------------------------------
 -- Utils
 
 pretty80 :: PP.Pretty a => a -> String
 pretty80 x = PP.displayS (PP.renderPretty 0.8 80 (PP.pretty x)) ""
-
-prettyOneLine :: PP.Pretty a => a -> String
-prettyOneLine x = PP.displayS (PP.renderCompact (PP.pretty x)) ""
