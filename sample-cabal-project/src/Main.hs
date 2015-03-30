509e671b4496d5c12f35f9d2ee5954d880ce0af9
Francesco Mazzoli <francesco@fpcomplete.com>
2015-03-30 16:40:51 +0200

    Remove "pure" C embeddings.
    For two reasons:
    
    * The user should be aware more clearly that embedding C code without
      `IO` is a highly unsafe operation.
    * Distinguishing between IO and non-IO inside the Context code
      makes things much more confusing.  If we add back a `_pure` versions,
      we should do it by adding an `unsafePerformIO` at the top-level, as
      you would manually.

--- sample-cabal-project/src/Main.hs
+++ sample-cabal-project/src/Main.hs
@@ -15,26 +15,6 @@ test_cexp_unsafe :: CDouble -> CDouble -> IO CDouble
 test_cexp_unsafe x y =
   [cexp_unsafe| double(double x, double y){ cos(x) + cos(y) } |]
 
-test_cexp_pure :: CDouble -> CDouble
-test_cexp_pure x =
-  [cexp_pure| double(double x){ cos(x) + sin(x) } |]
-
-test_cexp_pure_unsafe :: CDouble -> CDouble
-test_cexp_pure_unsafe x =
-  [cexp_pure_unsafe| double(double x){ cos(x) + sin(x) } |]
-
-test_suffixType1 :: CInt -> CInt -> CInt
-test_suffixType1 x _y = [cexp_pure| int{ $(int x) + $(int x) } |]
-
-test_suffixType2 :: CInt -> CInt -> CInt
-test_suffixType2 x y = [cexp_pure| int(){ $(int x) + $(int y) } |]
-
-test_suffixType3 :: CInt -> CInt -> CInt
-test_suffixType3 x y = [cexp_pure| int(int x){ x + $(int y) } |]
-
-test_suffixType4 :: CInt -> CInt -> CInt
-test_suffixType4 x y = [cexp_pure| int(int x){ $(int x) + $(int y) } |]
-
 test_voidExp :: IO ()
 test_voidExp = [cexp| void { printf("Hello\n") } |]
 
@@ -42,10 +22,4 @@ main :: IO ()
 main = do
   print =<< test_cexp 3 4
   print =<< test_cexp_unsafe 3 4
-  print $ test_cexp_pure 4
-  print $ test_cexp_pure_unsafe 4
-  print $ test_suffixType1 1 2
-  print $ test_suffixType2 1 2
-  print $ test_suffixType3 1 2
-  print $ test_suffixType4 1 2
   test_voidExp
