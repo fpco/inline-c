a0d799c11fa7d237813ef0f17b15ee3f68dfbc08
Francesco Mazzoli <francesco@fpcomplete.com>
2015-04-24 16:11:07 +0200

    Add `using` util to C++ module.

--- inline-c/src/Language/C/Inline/Cpp.hs
+++ inline-c/src/Language/C/Inline/Cpp.hs
@@ -4,14 +4,20 @@
 module Language.C.Inline.Cpp
   ( module Language.C.Inline
   , cppCtx
+  , using
   ) where
+
+import           Data.Monoid ((<>), mempty)
+import qualified Language.Haskell.TH as TH
 
 import           Language.C.Inline
 import           Language.C.Inline.Context
-import           Data.Monoid ((<>), mempty)
 
 cppCtx :: Context
 cppCtx = baseCtx <> mempty
   { ctxFileExtension = Just "cpp"
   , ctxOutput = Just $ \s -> "extern \"C\" {\n" ++ s ++ "\n}"
   }
+
+using :: String -> TH.DecsQ
+using s = literal $ "using " ++ s ++ ";"
