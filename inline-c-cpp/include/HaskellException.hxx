2d5d944ae12088369735d0ce5c5b48228e2a5c4b
Robert Hensing <robert@roberthensing.nl>
2019-08-27 11:44:14 +0200

    Add docs to C++ HaskellException class

--- inline-c-cpp/include/HaskellException.hxx
+++ inline-c-cpp/include/HaskellException.hxx
@@ -5,6 +5,16 @@
 #include <memory>
 #include <string>
 
+/* A representation of a Haskell exception (SomeException), with a precomputed
+   exception message from Control.Exception.displayException.
+
+   The std::exception requires that retrieving the message does not mutate the
+   exception object and does not throw exceptions.
+
+   This class uses std::shared_ptr for the exception, because its callers can
+   not know in advance where and how often the exception will be copied, or when
+   it is released.
+ */
 class HaskellException : public std::exception {
 public:
   std::shared_ptr<HaskellStablePtr> haskellExceptionStablePtr;
